ba2a6de70cdf8331b7530d3577bd3ce36c05da48
SStoklosa <stoklosasebastian144@proton.me>
2023-09-11 07:35:35 -0500

    Fixed code problem.

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def on_button_clicked(button):
     ssh_command = f"ssh {user}@{ip}"
 
     # Launch a new terminal window with the SSH command
-    subprocess.Popen(["gnome-terminal", "--", "/bin/bash", "-c", ssh_command])
+    subprocess.Popen(["/usr/bin/flatpak-spawn","gnome-terminal", "--", "/bin/bash", "-c", ssh_command])
 
 window = Gtk.Window(title="GSSH")
 window.connect("delete-event", Gtk.main_quit)
-window.set_default_size(400, 600)  # Set the window size to 800x600
+window.set_default_size(400, 300)  # Set the window size to 800x600
 
 # Create input fields
 ip_label = Gtk.Label(label="IP:")
