d896ae9a595526f69d49c0e4dddf5cf6ffbb8679
SStoklosa23 <stoklosasebastian144@proton.me>
2023-09-29 17:58:38 -0400

    Fixed flatpak problems

--- main.py
+++ main.py
@@ -5,10 +5,31 @@ gi.require_version('Gio', '2.0')
 from gi.repository import Gtk, Vte, GLib, Gdk, Gio
 import subprocess
 import os
-from ssh_key_manager import generate_ssh_key 
+import time
 # Initialize the GTK application
 app = Gtk.Application()
 
+def generate_ssh_key(key_name, file_path):
+    try:
+        # Ensure the chosen directory is in /home
+        if not file_path.startswith("/home"):
+            raise ValueError("SSH keys must be saved in /home directory")
+
+        # Ensure the file doesn't exist by appending a timestamp to the filename
+        timestamp = int(time.time())
+        unique_file_path = f"{file_path}_{timestamp}"
+
+        # Generate SSH keys without a passphrase
+        ssh_keygen_command = f"ssh-keygen -t rsa -b 4096 -N \"\" -C {key_name} -f {unique_file_path}"
+
+        subprocess.run(ssh_keygen_command, shell=True, check=True)
+
+        # Rename the generated file to the original filename
+        os.rename(unique_file_path, file_path)
+        
+        return True, None
+    except Exception as e:
+        return False, str(e)
 
 
 # Function to handle the "Generate SSH keys" button click event
