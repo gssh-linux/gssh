2bebe413c79acdf215942f086be72f49ef61c922
SStoklosa <93952788+SStoklosa23@users.noreply.github.com>
2024-01-22 14:22:59 -0300

    Update apply_extra.sh

--- Flatpak/apply_extra.sh
+++ Flatpak/apply_extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_IMAGE="/app/extra/lunar-client.appimage"
+APP_IMAGE="/app/extra/gssh-1.0.0.AppImage"
 
 # Allow image to execute
 
@@ -16,19 +16,7 @@ DEST="/app/extra/bin/"
 mkdir $DEST
 cp -r squashfs-root/* $DEST
 
-# Install icons
 
-ICON_DIR="/app/extra/export/share/icons/hicolor/"
-
-mkdir -p $ICON_DIR
-cp -r squashfs-root/usr/share/icons/hicolor/* $ICON_DIR
-
-iconSizes=("16" "32" "48" "64" "128" "256")
-
-for I in "${iconSizes[@]}"
-do
-	dir="$ICON_DIR/${I}x${I}/apps/"
-	mv "$dir/launcher.png" "$dir/com.lunarclient.LunarClient.png"
 done
 
 # Clean up
